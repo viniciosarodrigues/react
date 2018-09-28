ddfd171c79a4f92049a5b17034da751cafa65107
WPD\vinicios.rodrigues <viniciosarodrigues@gmail.com>
2018-09-28 15:25:14 -0300

    Implementação de Update e Persist de usuário

--- frontend/src/components/user/UserCRUD.jsx
+++ frontend/src/components/user/UserCRUD.jsx
@@ -17,6 +17,12 @@ const initialState = {
 export default class UserCRUD extends Component {
     state = {...initialState}
 
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({list: resp.data})
+        })
+    }
+
     clear() {
         this.setState({user: initialState.user})
     }
@@ -28,8 +34,14 @@ export default class UserCRUD extends Component {
         axios[method](url, user)
             .then(resp => {
                 const list = this.getUpdatedList(resp.data)
-                this.setState({user: initialState, list})
+                this.setState({user: initialState.user, list})
             })
+    }
+
+    updateField(e) {
+        const user = { ...this.state.user }
+        user[e.target.name] = e.target.value
+        this.setState({ user })
     }
 
     getUpdatedList(user) {
@@ -38,10 +50,44 @@ export default class UserCRUD extends Component {
         return list
     }
 
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" 
+                                className="form-control"
+                                name="name" placeholder="Informe um nome de usuário"
+                                value={this.state.user.name} 
+                                onChange={e => this.updateField(e)} />
+                        </div>                                                    
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="email" className="form-control" name="email" placeholder="Informe um email..."
+                                value={this.state.user.email} 
+                                onChange={e => this.updateField(e)} />
+                        </div>  
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>Salvar</button>
+                        <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuários
+                {this.renderForm()}
             </Main>
         )
     }
