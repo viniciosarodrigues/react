ef7749c695b12abfbba37bf672366ff287f6ff6f
WPD\vinicios.rodrigues <viniciosarodrigues@gmail.com>
2018-09-28 13:34:13 -0300

    Implementação do Save, Clear e Update no UserCrud

--- frontend/src/components/user/UserCRUD.jsx
+++ frontend/src/components/user/UserCRUD.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios  from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -7,7 +8,36 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Lista, Alterar e Excluir.'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: {name: '', email: ''},
+    list: []
+}
+
 export default class UserCRUD extends Component {
+    state = {...initialState}
+
+    clear() {
+        this.setState({user: initialState.user})
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({user: initialState, list})
+            })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
